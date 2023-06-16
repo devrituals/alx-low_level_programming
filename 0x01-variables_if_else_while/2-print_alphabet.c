a66e80fdb4e0171e3fc2c979b56ae06bdb311644
devrituals <abbelabbel9@gmail.com>
2023-06-16 12:37:07 -0700

    lets gooo!

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,17 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{         
+char alpha = 'a';
+while (alpha <= 'z')
 {
-         
-	char alpha = 'a';
-
-        while (alpha <= 'z')
-	{
-
-	       putchar(alpha);
-	       alpha++;
-	}
-	putchar('\n');
-
-	return (0);	
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);	
 }
