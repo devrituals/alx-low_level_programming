a14ad2081e896a35c52ae369a1ecec9a3e8dd5c7
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-08 02:18:59 +0100

    Update 0 to 100 files to 0x015-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,18 +10,17 @@
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
-	return (0);
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	return (0);
+		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
@@ -29,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-	free(buffer);
+		free(buffer);
 		return (0);
 	}
 
