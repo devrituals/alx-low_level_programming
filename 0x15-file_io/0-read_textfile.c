81a9671c2d7116b9d5b20037903ff3642c549c3b
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-08 02:38:36 +0100

    Update 0 to 100 files to 0x015-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,11 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-		return (0);
+	return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
+	return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+	free(buffer);
 		return (0);
 	}
 
